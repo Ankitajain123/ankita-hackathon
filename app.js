cc5a176497bc380e05dd5670ea30b7c9f832024b
Ankita <ankita@PT-MAC-25.local>
2018-02-01 10:53:28 +0530

    After changes

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-// /* MAKAAN GURGAON LISTING
-// */
-
 const express = require('express');
 	http = require('http');
 	bodyParser = require('body-parser');
@@ -8,7 +5,8 @@ const express = require('express');
     path = require('path');
     var request = require('request');
 
-    fs = require('fs');
+   
+var urlparse = require('url');
 
 
 var fetch = require('node-fetch');
@@ -28,106 +26,48 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.json({ limit: '100mb' }));
 app.use(cookieParser());
 
-let v='';
-let r='';
-let o='';
-let t='';
-let th='';
-let f='';
-url1='https://www.makaan.com/petra/app/v4/listing?selector={"fields":["localityId","displayDate","listing","property","project","builder","displayName","locality","suburb","city","state","currentListingPrice","companySeller","company","user","id","name","label","listingId","propertyId","projectId","propertyTitle","unitTypeId","resaleURL","description","postedDate","verificationDate","size","measure","bedrooms","bathrooms","listingLatitude","listingLongitude","studyRoom","servantRoom","pricePerUnitArea","price","localityAvgPrice","negotiable","rk","buyUrl","rentUrl","overviewUrl","minConstructionCompletionDate","maxConstructionCompletionDate","halls","facingId","noOfOpenSides","bookingAmount","securityDeposit","ownershipTypeId","furnished","constructionStatusId","tenantTypes","bedrooms","balcony","floor","totalFloors","listingCategory","possessionDate","activeStatus","type","logo","profilePictureURL","score","assist","contactNumbers","contactNumber","isOffered","mainImageURL","mainImage","absolutePath","altText","title","imageCount","geoDistance","defaultImageId","updatedAt","qualityScore","projectStatus","throughCampaign","addedByPromoter","listingDebugInfo","videos","imageUrl","rk","penthouse","studio","paidLeadCount","listingSellerTransactionStatuses","allocation","allocationHistory","masterAllocationStatus","status","sellerCompanyFeedbackCount","companyStateReraRegistrationId"],';
-url2='"filters":{"and":[{"equal":{"cityId":11}},{"equal":{"listingCategory":["Rental"]}}]},"paging":{"start":0,"rows":20}}&includeNearbyResults=false&includeSponsoredResults=false&sourceDomain=Makaan';
-url='https://www.makaan.com/petra/app/v4/listing?selector={"fields":["localityId","displayDate","listing","property","project","builder","displayName","locality","suburb","city","state","currentListingPrice","companySeller","company","user","id","name","label","listingId","propertyId","projectId","propertyTitle","unitTypeId","resaleURL","description","postedDate","verificationDate","size","measure","bedrooms","bathrooms","listingLatitude","listingLongitude","studyRoom","servantRoom","pricePerUnitArea","price","localityAvgPrice","negotiable","rk","buyUrl","rentUrl","overviewUrl","minConstructionCompletionDate","maxConstructionCompletionDate","halls","facingId","noOfOpenSides","bookingAmount","securityDeposit","ownershipTypeId","furnished","constructionStatusId","tenantTypes","bedrooms","balcony","floor","totalFloors","listingCategory","possessionDate","activeStatus","type","logo","profilePictureURL","score","assist","contactNumbers","contactNumber","isOffered","mainImageURL","mainImage","absolutePath","altText","title","imageCount","geoDistance","defaultImageId","updatedAt","qualityScore","projectStatus","throughCampaign","addedByPromoter","listingDebugInfo","videos","imageUrl","rk","penthouse","studio","paidLeadCount","listingSellerTransactionStatuses","allocation","allocationHistory","masterAllocationStatus","status","sellerCompanyFeedbackCount","companyStateReraRegistrationId"],"filters":{"and":[{"equal":{"cityId":11}},{"equal":{"listingCategory":["Primary","Resale"]}}]},"paging":{"start":0,"rows":20}}&includeNearbyResults=false&includeSponsoredResults=false&sourceDomain=Makaan';
-request(url,function(err,response,body){
-    v=JSON.parse(body);
-});
 
-request(url1+url2,function(err,response,body){
-    r=JSON.parse(body);
-});
-url2='"filters":{"and":[{"equal":{"cityId":11}},{"equal":{"bedrooms":1}},{"equal":{"listingCategory":["Primary","Resale"]}}]},"paging":{"start":0,"rows":20}}&includeNearbyResults=false&includeSponsoredResults=false&sourceDomain=Makaan';
+//url='https://www.makaan.com/petra/app/v4/listing?selector={"fields":["localityId","displayDate","listing","property","project","builder","displayName","locality","suburb","city","state","currentListingPrice","companySeller","company","user","id","name","label","listingId","propertyId","projectId","propertyTitle","unitTypeId","resaleURL","description","postedDate","verificationDate","size","measure","bedrooms","bathrooms","listingLatitude","listingLongitude","studyRoom","servantRoom","pricePerUnitArea","price","localityAvgPrice","negotiable","rk","buyUrl","rentUrl","overviewUrl","minConstructionCompletionDate","maxConstructionCompletionDate","halls","facingId","noOfOpenSides","bookingAmount","securityDeposit","ownershipTypeId","furnished","constructionStatusId","tenantTypes","bedrooms","balcony","floor","totalFloors","listingCategory","possessionDate","activeStatus","type","logo","profilePictureURL","score","assist","contactNumbers","contactNumber","isOffered","mainImageURL","mainImage","absolutePath","altText","title","imageCount","geoDistance","defaultImageId","updatedAt","qualityScore","projectStatus","throughCampaign","addedByPromoter","listingDebugInfo","videos","imageUrl","rk","penthouse","studio","paidLeadCount","listingSellerTransactionStatuses","allocation","allocationHistory","masterAllocationStatus","status","sellerCompanyFeedbackCount","companyStateReraRegistrationId"],"filters":{"and":[{"equal":{"cityId":11}},{"equal":{"listingCategory":["Primary","Resale"]}}]},"paging":{"start":0,"rows":20}}&includeNearbyResults=false&includeSponsoredResults=false&sourceDomain=Makaan';
+//let v='';
 
-request(url1+url2,function(err,response,body){
-    o=JSON.parse(body);
-});
-url2='"filters":{"and":[{"equal":{"cityId":11}},{"equal":{"bedrooms":2}},{"equal":{"listingCategory":["Primary","Resale"]}}]},"paging":{"start":0,"rows":20}}&includeNearbyResults=false&includeSponsoredResults=false&sourceDomain=Makaan';
-
-request(url1+url2,function(err,response,body){
-    t=JSON.parse(body);
-});
-url2='"filters":{"and":[{"equal":{"cityId":11}},{"equal":{"bedrooms":3}},{"equal":{"listingCategory":["Primary","Resale"]}}]},"paging":{"start":0,"rows":20}}&includeNearbyResults=false&includeSponsoredResults=false&sourceDomain=Makaan';
-
-request(url1+url2,function(err,response,body){
-    th=JSON.parse(body);
-});
-url2='"filters":{"and":[{"equal":{"cityId":11}},{"equal":{"bedrooms":4}},{"equal":{"bedrooms":5}},{"equal":{"bedrooms":6}},{"equal":{"bedrooms":7}},{"equal":{"listingCategory":["Primary","Resale"]}}]},"paging":{"start":0,"rows":20}}&includeNearbyResults=false&includeSponsoredResults=false&sourceDomain=Makaan';
-
-request(url1+url2,function(err,response,body){
-    f=JSON.parse(body);
-});
-
-
-
-
-// // Setup api routes
 app.get('/', gotError, function(req, res) {
+	var adr = req.url;
+	let jsonData = urlparse.parse(adr, true).query;
+    var bedroom = '';
+	let firstUrl = 'https://www.makaan.com/petra/app/v4/listing?';
+	let midUrl = 'selector={"fields":["localityId","displayDate","listing","property","project","builder","displayName","locality","suburb","city","state","currentListingPrice","companySeller","company","user","id","name","label","listingId","propertyId","projectId","propertyTitle","unitTypeId","resaleURL","description","postedDate","verificationDate","size","measure","bedrooms","bathrooms","listingLatitude","listingLongitude","studyRoom","servantRoom","pricePerUnitArea","price","localityAvgPrice","negotiable","rk","buyUrl","rentUrl","overviewUrl","minConstructionCompletionDate","maxConstructionCompletionDate","halls","facingId","noOfOpenSides","bookingAmount","securityDeposit","ownershipTypeId","furnished","constructionStatusId","tenantTypes","bedrooms","balcony","floor","totalFloors","listingCategory","possessionDate","activeStatus","type","logo","profilePictureURL","score","assist","contactNumbers","contactNumber","isOffered","mainImageURL","mainImage","absolutePath","altText","title","imageCount","geoDistance","defaultImageId","updatedAt","qualityScore","projectStatus","throughCampaign","addedByPromoter","listingDebugInfo","videos","imageUrl","rk","penthouse","studio","paidLeadCount","listingSellerTransactionStatuses","allocation","allocationHistory","masterAllocationStatus","status","sellerCompanyFeedbackCount","companyStateReraRegistrationId"],';
+	var rent = ',{"equal":{"listingCategory":["Primary","Resale"]}}';
+    
+    var sort='';
+    if(jsonData.listingType){
+            rent =  ',{"equal":{"listingCategory":["Rental"]}}'; }
 
-    res.render(('index.ejs'),{info: v.data[0].facetedResponse.items});
-});
+    if(jsonData.bhk){
+        	if(jsonData.bhk!=4)
+            	bedroom = ',{"equal":{"bedrooms":'+ jsonData.bhk +'}}';
+        	else
+        		bedroom = ',{"equal":{"bedrooms":[4, 5, 6, 7, 8, 9]}}';
+		}
+    var city ='{"and":[{"equal":{"cityId":11}}';
+    if(jsonData.cityid==18)
+        city='{"and":[{"equal":{"cityId":18}}';
+    
+    if(jsonData.sort){
+            sort = ' ,"sort":[{"field":"price","sortOrder":"' + jsonData.sort + '"}]';
+        }
 
-app.get('/rent', gotError, function(req, res) {
-    res.render(('index.ejs'),{info: r.data[0].facetedResponse.items});
-});
-app.get('/1bhk', gotError, function(req, res) {
-    res.render(('index.ejs'),{info: o.data[0].facetedResponse.items});
-});
-app.get('/2bhk', gotError, function(req, res) {
-    res.render(('index.ejs'),{info: t.data[0].facetedResponse.items});
-});
-app.get('/3bhk', gotError, function(req, res) {
-   res.render(('index.ejs'),{info: th.data[0].facetedResponse.items}); 
-});
-app.get('/4bhk', gotError,function(req, res) {
-    res.render(('index.ejs'),{info: f.data[0].facetedResponse.items});
-});
-url2='"filters":{"and":[{"equal":{"cityId":11}},{"equal":{"bedrooms":1}},{"equal":{"listingCategory":["Rental"]}}]},"paging":{"start":0,"rows":20}}&includeNearbyResults=false&includeSponsoredResults=false&sourceDomain=Makaan';
+    let midUrl2 = '"filters":'+ city + rent + bedroom+']}' + ',"paging":{"start":0,"rows":20}'+sort+'}';
+	let lastUrl='&includeNearbyResults=false&includeSponsoredResults=false&sourceDomain=Makaan';
+        
+        
+    finalUrl = firstUrl+midUrl+midUrl2+lastUrl;  
 
-request(url1+url2,function(err,response,body){
-    or=JSON.parse(body);
+    request(finalUrl,function(err,response,body){
+    	//console.log(finalUrl);
+    v=JSON.parse(body);
+    res.render(('index.ejs'),{info: v.data[0]});
+	});
 });
-url2='"filters":{"and":[{"equal":{"cityId":11}},{"equal":{"bedrooms":2}},{"equal":{"listingCategory":["Rental"]}}]},"paging":{"start":0,"rows":20}}&includeNearbyResults=false&includeSponsoredResults=false&sourceDomain=Makaan';
-
-request(url1+url2,function(err,response,body){
-    tr=JSON.parse(body);
-});
-url2='"filters":{"and":[{"equal":{"cityId":11}},{"equal":{"bedrooms":3}},{"equal":{"listingCategory":["Rental"]}}]},"paging":{"start":0,"rows":20}}&includeNearbyResults=false&includeSponsoredResults=false&sourceDomain=Makaan';
-
-request(url1+url2,function(err,response,body){
-    thr=JSON.parse(body);
-});
-
-url2='"filters":{"and":[{"equal":{"cityId":11}},{"equal":{"bedrooms":4}},{"equal":{"bedrooms":5}},{"equal":{"bedrooms":6}},{"equal":{"bedrooms":7}},{"equal":{"listingCategory":["Rental"]}}]},"paging":{"start":0,"rows":20}}&includeNearbyResults=false&includeSponsoredResults=false&sourceDomain=Makaan';
-
-request(url1+url2,function(err,response,body){
-    fr=JSON.parse(body);
-});
-
-
-
-app.get('/rent/1bhk', gotError, function(req, res) {
-    res.render(('index.ejs'),{info: or.data[0].facetedResponse.items});
-});
-app.get('/rent/2bhk', gotError, function(req, res) {
-    res.render(('index.ejs'),{info: tr.data[0].facetedResponse.items});
-});
-app.get('/rent/3bhk', gotError, function(req, res) {
-    res.render(('index.ejs'),{info: thr.data[0].facetedResponse.items});
-});
-app.get('/rent/4bhk', gotError, function(req, res) {
-    res.render(('index.ejs'),{info: fr.data[0].facetedResponse.items});
-});
-
-
-
 
 function gotError(err, req, res, next) {
     console.log("error", err);
